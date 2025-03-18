7972bdfb4dcd4e26486d498765874ac7133cd9ad
ShiyoDawn <forever2h8csq@163.com>
2025-03-18 18:57:15 +0800

    alusys situation resolved

--- design/simulation/qsim/work/alucomb/_primary.vhd
+++ design/simulation/qsim/work/alucomb/_primary.vhd
@@ -8,6 +8,6 @@ entity alucomb is
         A               : in     vl_logic_vector(7 downto 0);
         B               : in     vl_logic_vector(7 downto 0);
         Op              : in     vl_logic_vector(1 downto 0);
-        Q               : out    vl_logic_vector(7 downto 0)
+        F               : out    vl_logic_vector(7 downto 0)
     );
 end alucomb;
