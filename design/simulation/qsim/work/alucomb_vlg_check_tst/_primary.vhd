88c85b47b50380085285b3d64779f0dcf5385566
ShiyoDawn <forever2h8csq@163.com>
2025-03-18 18:57:15 +0800

    alusys situation resolved

--- design/simulation/qsim/work/alucomb_vlg_check_tst/_primary.vhd
+++ design/simulation/qsim/work/alucomb_vlg_check_tst/_primary.vhd
@@ -3,7 +3,7 @@ use verilog.vl_types.all;
 entity alucomb_vlg_check_tst is
     port(
         Co              : in     vl_logic;
-        Q               : in     vl_logic_vector(7 downto 0);
+        F               : in     vl_logic_vector(7 downto 0);
         sampler_rx      : in     vl_logic
     );
 end alucomb_vlg_check_tst;
