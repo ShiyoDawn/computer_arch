a07e4e302b9e619dc3bd833c4a630dba8f3462cb
ShiyoDawn <forever2h8csq@163.com>
2025-04-02 20:24:45 +0800

    follow video

--- design/simulation/qsim/work/memtest/_primary.vhd
+++ design/simulation/qsim/work/memtest/_primary.vhd
@@ -2,14 +2,18 @@ library verilog;
 use verilog.vl_types.all;
 entity memtest is
     port(
-        Q               : out    vl_logic_vector(7 downto 0);
-        CLK             : in     vl_logic;
-        R               : in     vl_logic;
-        W               : in     vl_logic;
+        mA              : out    vl_logic_vector(7 downto 0);
         aSEL            : in     vl_logic;
         CNT             : in     vl_logic;
+        CLK             : in     vl_logic;
         RST             : in     vl_logic;
         wMAR            : in     vl_logic;
-        wMBR            : in     vl_logic
+        Q               : out    vl_logic_vector(7 downto 0);
+        W               : in     vl_logic;
+        R               : in     vl_logic;
+        wMBR            : in     vl_logic;
+        MAR             : out    vl_logic_vector(7 downto 0);
+        MBR             : out    vl_logic_vector(7 downto 0);
+        PC              : out    vl_logic_vector(7 downto 0)
     );
 end memtest;
--- design/simulation/qsim/work/memtest_vlg_check_tst/_primary.vhd
+++ design/simulation/qsim/work/memtest_vlg_check_tst/_primary.vhd
@@ -2,6 +2,10 @@ library verilog;
 use verilog.vl_types.all;
 entity memtest_vlg_check_tst is
     port(
+        mA              : in     vl_logic_vector(7 downto 0);
+        MAR             : in     vl_logic_vector(7 downto 0);
+        MBR             : in     vl_logic_vector(7 downto 0);
+        PC              : in     vl_logic_vector(7 downto 0);
         Q               : in     vl_logic_vector(7 downto 0);
         sampler_rx      : in     vl_logic
     );
